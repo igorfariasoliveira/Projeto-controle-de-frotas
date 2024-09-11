eefe5a1285df43bcd8e93c578e9507a31036cee7
igorfariasoliveira <igor10farias@gmail.com>
2024-09-11 10:05:31 -0300

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function Login() {
   const [username, setUsername] = useState('');
-  const [password, serPassword] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,5 +17,13 @@ return(
     onChange={(e) => setUsername(e.target.value)}
     placeholder="Usuário" 
     />
+    <input 
+    type="password"
+    value={password} 
+    onChange={(e) => setPassword(e.target.value)}
+    placeholder="Senha"
+    />
+
+    <button type="submit">Login</button>
   </div>
-)
+);
