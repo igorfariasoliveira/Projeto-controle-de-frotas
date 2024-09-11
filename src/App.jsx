1e4c99f2b3150219ddb72bec83ffadc6113b07fe
igorfariasoliveira <igor10farias@gmail.com>
2024-09-11 11:09:03 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import Login from './components/Login'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-function App(){
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div>
-      <h1>Teste</h1>
-      <Login/>
-    </div>
+   
   )
 }
-export default App;
+
+export default App
